05816294387c3e728d404372889f76f2fe42ff61
Tony <lam_273@hotmail.com>
2023-03-14 12:33:47 -0500

    feature/ carousel change animation initial-not-working

--- src/server/js/api.mjs
+++ src/server/js/api.mjs
@@ -16,25 +16,62 @@ let apiRoute = function (app) {
 
     app.route('/').get(async (req, res) => {
         let discount_list = await Products.aggregate([
-            {
-                $match: {
-                    discount: true
-                }
-            },
+            {$match: { discount: true }},
             {
                 $project: {
                     name: 1,
-                    price: 1,
-                    discount_percent: 1
+                    formattedPrice: {
+                        $concat: [
+                            "  $",
+                            { $toString: { $round: ["$price", 2] } },
+                            {
+                                $cond: [
+                                    {
+                                        $regexMatch: {
+                                            input: { $toString: '$price' },
+                                            regex: /\./
+                                        }
+                                    }, "", ".00"
+                                ]
+                            }
+                        ]
+                    },
+                    discount_percent: 1,
+                    description: 1,
+                    price_discounted: {
+                        $concat: [" $", { $toString: { $round: [{ $subtract: ['$price', { $multiply: ['$price', '$discount_percent'] }] }, 2] } }, {
+                            $cond: [
+                                {
+                                    $regexMatch: {
+                                        input: { $toString: '$price' },
+                                        regex: /\./
+                                    }
+                                }, "", ".00"
+                            ]
+                        }]
+                    }
                 }
             }
         ]);
+
+        let empty_list = []
         res.render('home', { discount_list })
     });
 
     app.route('/test').get(async (req, res) => {
-
-        res.send('testerino');
+        /*
+        let agregar = Products.create({
+            "name": "testproduct7",
+            "description": "description testproduct7",
+            "price": 50,
+            "discount": true,
+            "discount_percent": 0.525,
+            "featured": true,
+            "tags": ["product", "test product"]
+        })
+        res.json(agregar);
+        */
+        res.send('aloha')
     })
 
 
