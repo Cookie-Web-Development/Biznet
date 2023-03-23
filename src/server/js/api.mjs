19e4c0e1d40d989a1a9fd2f255aa69517ff3cc30
Tony <lam_273@hotmail.com>
2023-03-23 12:39:54 -0500

    styling/ carousel & product-card 'medium' styled finished v1

--- src/server/js/api.mjs
+++ src/server/js/api.mjs
@@ -40,7 +40,7 @@ let apiRoute = function (app) {
             item.format_price = item.price.toLocaleString('en-US', formatOptions);
             item.format_price_discounted = item.price_discounted.toLocaleString('en-US', formatOptions);
         })
-        featured_list = [featured_list[0]];
+        //featured_list = [featured_list[0]];
         //console.log(featured_list)
         res.render('home', { discount_list, featured_list })
     });
