5410236a77dc7a786178d5bfdc109e243ac6e4fa
Tony <lam_273@hotmail.com>
2023-06-25 13:00:15 -0500

    savepoint/ since W10 crashed on me, imma be commting every change I get
    lunch break save

--- public/js/drag_display.js
+++ public/js/drag_display.js
@@ -1,47 +1,71 @@
 //## Drag Display
 let drag_btn = Array.from(document.querySelectorAll('[data-drag-btn]'));
-let drag_active = false; //ensures that the dragging event fires and stops when required
+let drag_active = false; //touch-click-drag: ensures that the dragging event fires and stops when required
 
 drag_btn.forEach(button => {
     let display_target = document.querySelector(`[data-drag-display=${button.dataset.dragBtn}]`);
     let max_distance_travel = display_target.scrollWidth;
     let visible_window = display_target.clientWidth;
     
+    //return if not enough items present.
     if (max_distance_travel <= visible_window) { return };
 
     button.classList.add('active');
 
+    //Slider indicator
+    let slider_indicator = document.querySelector(`[data-drag-indicator=${button.dataset.dragBtn}]`);
+
+    let indicator_width = ((visible_window / max_distance_travel) * 100).toFixed(2);  
+    slider_indicator.style.width = `${indicator_width}%`;
+
+
     button.addEventListener('click', () => {
-        draggingFunction(display_target, button.dataset.function, visible_window, max_distance_travel)
+        draggingFunction(display_target, button, slider_indicator, visible_window, max_distance_travel)
     })
 })
 
 
-function draggingFunction (target, direction, distance_to_travel, max_distance) {
+function draggingFunction (target, button, slider, distance_to_travel, max_distance) {
     if (!(typeof max_distance === 'number' || typeof distance_to_travel === 'number')) { return };
 
     //currernt_x MUST NOT be higher than 0
     let current_x = getComputedTransformX(target);
     let travel, steps, progress;
+
     //this compensates the gap between elements into the dragging distance.
     let gap_between = parseFloat(window.getComputedStyle(target).gap) || 0;
-    let distance_with_gap = distance_to_travel - (gap_between / 2);
-    
+    let distance_with_gap = distance_to_travel + (gap_between / 4);
+   
+    let slider_current_x = parseFloat(slider.style.left) || 0;
+    let slider_travel = parseFloat(slider.style.width), slider_step;
 
-    switch(direction) {
+    //TAKING A BREAK NOTE: Lets make use of left and percentage!!
+
+    switch(button.dataset.function) {
         case "next":
             //distance validator: checks if there is enough. Else, set to distance_to_travel - max_distance
             current_x - (distance_with_gap * 2) < -max_distance ? 
-            travel = distance_with_gap - max_distance :
+
+            travel = distance_with_gap - max_distance - (gap_between / 2) :
+
             travel = current_x - distance_with_gap ;
+
             steps = -100;
+
             break;
         case "previous":
-            //distance validator: checks if there is enough. Else, set to 0
-            current_x + (distance_with_gap * 2) > 0 ?
-            travel = 0 :
+            //distance validator: checks if there is enough. Else, set to 0. No idea why this has to be 1.9 but it works as is.
+            current_x + (distance_with_gap * 1.9) > 0 ?
+
+            travel = 0 + (gap_between / 4) :
+
             travel = current_x + distance_with_gap ;
+
             steps = 100;
+
+            //slider_travel = -(parseFloat(getComputedStyle(slider).width));
+            //slider_step = -50;
+
             break;
         default: 
             return;
@@ -52,15 +76,16 @@ function draggingFunction (target, direction, distance_to_travel, max_distance) 
     function animation () {
         current_x = current_x + steps;
         progress = progress - (Math.abs(steps));
-
+        
         let transform_value = `translateX(${current_x}px)`;
-        target.style.transform = transform_value;
+        target.style.transform = transform_value;        
 
         if(progress > 0) {
             requestAnimationFrame(animation)
         } else {
             transform_value = `translateX(${travel}px)`;
             target.style.transform = transform_value;
+
         }
     }
 
