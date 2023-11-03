b911673a589dde1ca161eaaa65e56179c09153c3
Tony <lam_273@hotmail.com>
2023-11-03 15:54:16 -0500

    css/ linked all pugs with their respective css
    updated index.css to inlcude css that are also globally used
    
    pending more testing to find graphical issues and load times. Block loading should not be an issue.

--- src/client/components/profile_nav.js
+++ src/client/components/profile_nav.js
@@ -4,5 +4,9 @@ let route = path.match(route_regex);
 
 let active_li = route[0];
 
+if (active_li === '/profile/password') {
+    active_li = '/profile'
+}
+
 let active_menu = document.querySelector(`[data-profile-active='${active_li}']`)
 active_menu.classList.add('active')
